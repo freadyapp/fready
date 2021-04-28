b8edc5a3c6fa3ad2c513a0be3d668a30a854dc7a
robo-monk <rrobomonk@gmail.com>
2021-04-28 14:16:57 +0200

    injected chrome extension have a nice read

--- app/scripts/xfreadyBase.js
+++ app/scripts/xfreadyBase.js
@@ -1,4 +1,31 @@
-chrome.runtime.sendMessage("command:inject", (response) => {
-    console.log("hello from xfready!")
-    console.log(response)
-})
+function __onDocLoad__() {
+    chrome.runtime.sendMessage("command:inject", (response) => {
+        // let name 
+        chrome.storage.sync.get('user', data => {
+            let name = data.user.name
+
+console.log(`%c
+    @@@@@@@@@@@@@@@@
+    @@@@@@@@@@@@@@
+    @@@@@@@@@@@.
+    @@@@@@@@@
+    @@@@@@  (@@@@@@
+    @@@@    (@@@,
+    @.      (@
+
+
+   🌴 injected the Fready Chrome Extension!
+      Have a nice read${name ? `, ${name}` : ''}!
+`, "border-left: 2px solid #2b6cce;"
+            )
+        })
+    })
+}
+
+if (document.readyState === "complete") {
+    __onDocLoad__()
+} else {
+    document.addEventListener('readystatechange', () => {
+        if (document.readyState === "complete") __onDocLoad__()
+    })
+}
